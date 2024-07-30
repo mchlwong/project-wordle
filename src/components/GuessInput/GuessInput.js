8d58ac16359cc9da0811f1adc0c8b6307b5183a0
Michael Wong <ofmchl@gmail.com>
2024-07-30 14:55:51 +0800

    Exercise 1: GuessInput

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function GuessInput() {
+  const [word, setWord] = React.useState('');
+  
+  function handleChange(e) {
+    setWord(e.target.value.toUpperCase());
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(word);
+    setWord('');
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input id="guess-input" type="text" pattern="[A-Z]{5,5}" value={word} onChange={handleChange} required={true} />
+    </form>
+  );
+}
+
+export default GuessInput;
